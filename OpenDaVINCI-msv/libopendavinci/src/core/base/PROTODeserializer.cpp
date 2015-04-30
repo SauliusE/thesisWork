fcda2f2d2d2bd366a570063815615353a21639ea
Saulius Eidukas <s.eidukas@gmail.com>
2015-04-30 14:04:31 +0200

    updated protobuf serializer

--- OpenDaVINCI-msv/libopendavinci/src/core/base/PROTODeserializer.cpp
+++ OpenDaVINCI-msv/libopendavinci/src/core/base/PROTODeserializer.cpp
@@ -16,197 +16,166 @@ uint32_t decodeVar ( istream &in, uint64_t &value );
                 m_buffer(),
                 m_values(),
                 m_size(0),
-        		position(0){
+                position(0){
             // Initialize the stringstream for getting valid positions when calling tellp().
             // This MUST be a blank (Win32 has a *special* implementation...)!
  
-              	//Decoding magic number
-		uint64_t value = 0;
-	        decodeVar(in,value);
-		//casting value to uint16
-		uint16_t magicNumber = (uint16_t) value;
-		
-		in.clear();
-		in.seekg(0, ios::beg);
-		//cout << "Decoded Magic Number" << magicNumber<< endl;
-		if(magicNumber == 0xAABB){
-		  cout << "magicNumber" << endl;
-		  return;
-        }
+                  //Decoding magic number
+            uint64_t value = 0;
+            decodeVar(in,value);
+            //casting value to uint16
+            uint16_t magicNumber = (uint16_t) value;
         
-		
-		
-		char c = 0;
-		in.get(c);
-	
-		while(in.good() ){
-			m_buffer.put(c);
-			in.get(c);
-			
-		}
-		in.clear();
-		in.seekg(0,ios_base::beg);
+            in.clear();
+            in.seekg(0, ios::beg);
+            if(magicNumber == 0xAABB){
+                return;
+            }
+        
+        
+        
+            char c = 0;
+            in.get(c);
+        
+            while(in.good() ){
+                m_buffer.put(c);
+                in.get(c);
+                
+            }
+            
+            in.clear();
+            in.seekg(0,ios_base::beg);
 
         }
 
         PROTODeserializer::~PROTODeserializer() { 
-		}
+        }
        
 
         void PROTODeserializer::read(const uint32_t id, Serializable &s) {
-		  (void) id;
-				m_buffer >> s;
-			}
+          (void) id; //to be removed in the future
+                m_buffer >> s;
+            }
 
         
 
         
         void PROTODeserializer::read(const uint32_t id, bool &b) {
-		  		  (void)id;
-       //                   void* _b = (bool*) &b;
+                (void)id;
+                uint64_t key;
+                decodeVar(m_buffer,key);
+           
+                WIRE_TYPE wireType = getWireType(key);
+                uint32_t fieldId = getFieldNumber(key);
+                (void) wireType;
+                (void) fieldId;
+                uint64_t v;
+                decodeVar(m_buffer,v);
 
-		  uint64_t key;
-		  decodeVar(m_buffer,key);
-          cout << " bool key Deserializer -------------------------" << key;
-		  WIRE_TYPE wireType = getWireType(key);
-		  uint32_t fieldId = getFieldNumber(key);
-		  (void) wireType;
-		  (void) fieldId;
-		  uint64_t v;
-		  int boolSize = decodeVar(m_buffer,v);
-          cout << "boolean size " << boolSize <<endl;
-          cout << "decoded value " << v;
-	//	  b = ( bool ) v;
-           b = v;
-           //b = ;
-          cout << "bool after casting " << b << endl;
-            //  cout << "Read Bool Deserializer" << "- ID value: " << id <<" | Bool value" << b <<endl ;
+                b = v;
         }
 
         void PROTODeserializer::read(const uint32_t id, char &c) {
-		  		  (void)id;
+                (void)id;
+                
+                    
+                uint64_t key;
+                decodeVar(m_buffer,key);                   
+                WIRE_TYPE wireType = getWireType(key);           
+                uint32_t fieldId = getFieldNumber(key);                   
+                (void) wireType;                  
+                (void) fieldId;                  
+                uint64_t v;                   
+                decodeVar(m_buffer,v);                   
+                c =  v;
+      }
 
-		  uint64_t key;
-		  decodeVar(m_buffer,key);
-		  WIRE_TYPE wireType = getWireType(key);
-		  uint32_t fieldId = getFieldNumber(key);
-		  (void) wireType;
-		  (void) fieldId;
-		  uint64_t v;
-		 uint32_t size = decodeVar(m_buffer,v);
-         cout << " char size "<< size << endl;
-          cout << "char value : " << v <<endl;
-		  c =  v;
-          cout << "after casting " << c <<endl;
-  //            cout << "Read char Deserializer" << "- ID value: " << id <<" | char  value: " << c <<endl;
-        }
+        void PROTODeserializer::read(const uint32_t id, unsigned char &uc) {          (void)id;
 
-        void PROTODeserializer::read(const uint32_t id, unsigned char &uc) {		  (void)id;
-
-         //    cout << "Read unsgined char Deserializer" << "- ID value: " << id <<"  | unsigned char  value: " << uc <<endl;
-			 uint64_t key;
-		  decodeVar(m_buffer,key);
-		  WIRE_TYPE wireType = getWireType(key);
-		  uint32_t fieldId = getFieldNumber(key);
-		  (void) wireType;
-		  (void) fieldId;
-		  uint64_t v;
-		  decodeVar(m_buffer,v);
-		  uc =  v;
+                uint64_t key;
+                decodeVar(m_buffer,key);
+                WIRE_TYPE wireType = getWireType(key);
+                uint32_t fieldId = getFieldNumber(key);
+                (void) wireType;
+                (void) fieldId;
+                uint64_t v;
+                decodeVar(m_buffer,v);
+                uc =  v;
 
         }
 
         void PROTODeserializer::read(const uint32_t id, int32_t &i) {
-        //     cout << "Read int32_t Deserializer" << "- ID value: " << id <<" | int32_t  value: " << i <<endl;
-		  		  (void)id;
+                (void)id;
 
-		  uint64_t key;
-		  decodeVar(m_buffer,key);
-		  WIRE_TYPE wireType = getWireType(key);
-		  uint32_t fieldId = getFieldNumber(key);
-		  (void) wireType;
-		  (void) fieldId;
-		  uint64_t v;
-		  decodeVar(m_buffer,v);
-		  i = (int32_t) v;
+                uint64_t key;
+                decodeVar(m_buffer,key);
+                WIRE_TYPE wireType = getWireType(key);
+                uint32_t fieldId = getFieldNumber(key);
+                (void) wireType;
+                (void) fieldId;
+                uint64_t v;
+                decodeVar(m_buffer,v);
+                i = (int32_t) v;
 
         }
 
         void PROTODeserializer::read(const uint32_t id, uint32_t &ui) {
-		  (void)id;
-      //      cout << "Read uint32_t Deserializer" << "- ID value: " << id <<" | uint32_t  value: " << ui <<endl;
-			uint64_t key;
-		  decodeVar(m_buffer,key);
-		  WIRE_TYPE wireType = getWireType(key);
-		  uint32_t fieldId = getFieldNumber(key);
-		  (void) wireType;
-		  (void) fieldId;
-		  
-		  uint64_t v;
-		  decodeVar(m_buffer,v);
-		  cout <<endl << " before conv " <<  v <<endl;
-		  ui = (uint32_t) v;
+                (void)id;
+                uint64_t key;
+                decodeVar(m_buffer,key);
+                WIRE_TYPE wireType = getWireType(key);
+                uint32_t fieldId = getFieldNumber(key);
+                (void) wireType;
+                (void) fieldId;
+                
+                uint64_t v;
+                decodeVar(m_buffer,v);
+                ui = (uint32_t) v;
         }
 
         void PROTODeserializer::read(const uint32_t id, float &f) {
-         //    cout << "Read float Deserializer" << "- ID value: " << id <<" < | float   value: " << f <<endl;
-			(void) id;  
-            uint64_t key;
-          decodeVar(m_buffer,key);
-          WIRE_TYPE wireType = getWireType(key);
-          uint32_t fieldId = getFieldNumber(key);
-          (void) wireType;
-          (void) fieldId;
-    //      char* value= new char[4];
-      //    m_buffer.read(value,4);
-        //   cout << " float value " << value <<endl;
-         //string floats(value);
-          // f=atof(floats.c_str());
-      //    f = (float) value;
-          float _f =0;
-             m_buffer.read(reinterpret_cast<char *>(&_f), 4);
-f= _f;
-                     cout << "afte casting f value " << _f <<endl;
-
+                (void) id;  
+                uint64_t key;
+                decodeVar(m_buffer,key);
+                WIRE_TYPE wireType = getWireType(key);
+                uint32_t fieldId = getFieldNumber(key);
+                (void) wireType;
+                (void) fieldId;
+  
+                float _f =0;
+                m_buffer.read(reinterpret_cast<char *>(&_f), 4);
+                f= _f;
+              
         }
 
         void PROTODeserializer::read(const uint32_t id, double &d) {
-            (void) id;
-            uint64_t key;
-decodeVar(m_buffer,key);
-          WIRE_TYPE wireType = getWireType(key);
-          uint32_t fieldId = getFieldNumber(key);
-          (void) wireType;
-          (void) fieldId;
-//           char* value= new char[8];
-// 
-//           m_buffer.read(value,8);
-//           cout << " double value " << value <<endl;
-//           string doubleValue(value);
-//           d = atof(doubleValue.c_str());
-//           cout << "afte casting d value " << d <<endl;
-          double _d =0;
-        m_buffer.read(reinterpret_cast<char *>(&_d),8);
-        d = _d;
-        cout << "double value !!!!!" << _d <<endl;
-
+                (void) id;
+                uint64_t key;
+                decodeVar(m_buffer,key);
+                WIRE_TYPE wireType = getWireType(key);
+                uint32_t fieldId = getFieldNumber(key);
+                (void) wireType;
+                (void) fieldId;                
+          
+                double _d =0;        
+                m_buffer.read(reinterpret_cast<char *>(&_d),8);        
+                d = _d;
         }
 
         void PROTODeserializer::read(const uint32_t id, string &s) {
-            (void) id;
-            uint64_t key;
-          decodeVar(m_buffer,key);
-          WIRE_TYPE wireType = getWireType(key);
-          uint32_t fieldId = getFieldNumber(key);
-          (void) wireType;
-          (void) fieldId;
-            uint64_t size;
-            decodeVar (m_buffer, size );
-            uint32_t length = (uint32_t) size;
-            char *str = new char[length];
-           m_buffer.read(str, length+1);
-           s = string(str, length);
-
+                (void) id;
+                uint64_t key;
+                decodeVar(m_buffer,key);
+                WIRE_TYPE wireType = getWireType(key);
+                uint32_t fieldId = getFieldNumber(key);
+                (void) wireType;
+                (void) fieldId;
+                uint64_t size;
+                decodeVar (m_buffer, size );
+                uint32_t length = (uint32_t) size;
+                char *str = new char[length];
+                m_buffer.read(str, length);
+                s = string(str, length);
         }
 
         void PROTODeserializer::read(const uint32_t id, void *data, uint32_t size) {
@@ -214,30 +183,41 @@ decodeVar(m_buffer,key);
 
         }
         void PROTODeserializer::read(istream &in, core::data::Container &container) {
-			  // Getting message size
-	       	//Decoding magic number
-		uint64_t value = 0;
-	        decodeVar(in,value);
-		//casting value to uint16
-		uint16_t magicNumber = (uint16_t) value;
-		cout << "Decoded Magic Number" << magicNumber<< endl;
-		value  = 0 ;	
-		decodeVar(in,value);
-		uint16_t dataType = (uint16_t) value;
-		cout << "decoded data type " << dataType << endl;
-		container.m_dataType = static_cast<core::data::Container::DATATYPE>(dataType);
-		uint64_t size;
-    		decodeVar(in, size);
-	       	uint64_t readSize = 0;
-		char c = 0;
-		in.get(c);
-		cout << "message length " << size <<endl;
-		while(in.good() && readSize < size){
-			m_buffer.put(c);
-			in.get(c);
-			readSize++;
-		}
-      		container.m_serializedData.str(m_buffer.str());
+                // Getting message size
+                // Decoding magic number
+                uint64_t value = 0;
+                decodeVar(in,value);
+                
+                // casting value to uint16
+                uint16_t magicNumber = (uint16_t) value;
+                if (magicNumber != 0xAABB){
+                   if (in.good()) {
+                    // Stream is good but still no magic number?
+                    clog << "Stream corrupt: magic number not found." << endl;
+                    }
+                return;                   
+                }
+                
+                value  = 0 ;    
+                decodeVar(in,value);
+                
+                uint16_t dataType = (uint16_t) value;
+                container.setDataType(static_cast<core::data::Container::DATATYPE>(dataType));
+                uint64_t size;
+                
+                decodeVar(in, size);
+                uint64_t readSize = 0;
+                
+                char c = 0;
+                in.get(c);
+                
+                while(in.good() && readSize < size){
+                    m_buffer.put(c);
+                    in.get(c);
+                    readSize++;
+                }
+                
+                container.m_serializedData.str(m_buffer.str());
 
     }
      
@@ -260,5 +240,5 @@ decodeVar(m_buffer,key);
               return size;
           }
 
-	}
+    }
 } // core::base
--- OpenDaVINCI-msv/libopendavinci/src/core/base/PROTOSerializer.cpp
+++ OpenDaVINCI-msv/libopendavinci/src/core/base/PROTOSerializer.cpp
@@ -25,259 +25,203 @@ namespace core {
         PROTOSerializer::~PROTOSerializer(){
             // Here suppose to be finalized message based on PROTO approach, as far i know it is
             // It is : 
-        	// - > size of message
-        	// - > payload
-			// Writing size of message
-		  //writing buffer to m_out
-		  		m_out << m_buffer.str();
-				
-
+            // - > size of message
+            // - > payload
+            // Writing size of message
+            m_out << m_buffer.str();
+       
         } // end of ~constructor
 
         void PROTOSerializer::write ( const uint32_t id, const Serializable& s ) {
              // writing serializable
-		  // ?? PURPOSE OF THIS FUNCTION
-		  (void)id;
-             stringstream buffer;
-             buffer << s;
-			 m_buffer << buffer.str();
-			  //cout<< "serializable" <<endl;
-
+            (void)id;
+            stringstream buffer;
+            buffer << s;
+            m_buffer << buffer.str();
         }
     
     
         void PROTOSerializer::write ( const uint32_t id, const bool& b ) {
-          //cout<< " Serialization of BOOLEAN. "<< endl;
-          // writing boolean (bool is being encoded as Varint)
-           //     uint64_t _b =  b ;
                 uint32_t sizeOFB = getVarSize(b);
-                cout << "boolean size " << sizeOFB << endl;
-				m_size += sizeOFB;
-				PROTO_TYPE protoType = ( PROTO_TYPE )6;
-				WIRE_TYPE wireType = getWireType ( protoType) ;
-				uint32_t key = getKey ( id, wireType );
-				m_size += getVarSize(key);
-                          cout << " serializer bool key --------------------------- " << key;
-
-				encode(m_buffer,key);
-                cout << "bool before Encoding " << b <<endl;
-				encode(m_buffer, b );
-				cout << "bool " << b <<endl;
-
-
+                m_size += sizeOFB;
+                
+                PROTO_TYPE protoType = ( PROTO_TYPE )6;
+                WIRE_TYPE wireType = getWireType ( protoType) ;
+                
+                uint32_t key = getKey ( id, wireType );
+                m_size += getVarSize(key);
+  
+                encode(m_buffer,key);
+                encode(m_buffer, b );
         }
         
         void PROTOSerializer::write ( const uint32_t id, const char& c ) {
-                  //cout<< " Serialization  of char. " <<endl;
-             //   uint64_t _c =  c ;
-				m_size += getVarSize(c);
-				PROTO_TYPE protoType = ( PROTO_TYPE )6;
-				WIRE_TYPE wireType = getWireType ( protoType) ;
-				uint32_t key = getKey ( id, wireType );
-				m_size += getVarSize(key);
-				encode(m_buffer,key);
-				encode(m_buffer,c );
-				cout << "char " << c <<endl;
-							  //cout<< "char" <<endl;
-
-		}
+               
+                m_size += getVarSize(c);
+                
+                PROTO_TYPE protoType = ( PROTO_TYPE )6;
+                WIRE_TYPE wireType = getWireType ( protoType) ;
+                
+                uint32_t key = getKey ( id, wireType );           
+                m_size += getVarSize(key);
+                
+                encode(m_buffer,key);
+                encode(m_buffer,c );
+        }
         
         void PROTOSerializer::write ( const uint32_t id, const unsigned char& uc ) {
-            //cout<< " Serialization of  unsigned char : "  << endl;
-       //         uint64_t _uc =  uc ;
-				m_size += getVarSize(uc);
-				PROTO_TYPE protoType = ( PROTO_TYPE )6;
-				WIRE_TYPE wireType = getWireType ( protoType) ;
-				uint32_t key = getKey ( id, wireType );
-				m_size += getVarSize(key);
-				encode(m_buffer,key);
-				encode(m_buffer, uc );
-								//cout << "u char " << _uc <<endl;
-
-					  //cout<< " unsigned char" <<endl;
-
+               
+                m_size += getVarSize(uc);
+                
+                PROTO_TYPE protoType = ( PROTO_TYPE )6;
+                WIRE_TYPE wireType = getWireType ( protoType) ;
+                
+                uint32_t key = getKey ( id, wireType );
+                m_size += getVarSize(key);
+                
+                encode(m_buffer,key);
+                encode(m_buffer, uc );
         }
 
         void PROTOSerializer::write ( const uint32_t id, const int32_t& i ) {
-          //cout<< "Serialization of int32 " <<endl;
-                uint64_t _i =  i ;
-				m_size += getVarSize(_i);
-				PROTO_TYPE protoType = ( PROTO_TYPE )0;
-				WIRE_TYPE wireType = getWireType ( protoType) ;
-				uint32_t key = getKey ( id, wireType );
-				m_size += getVarSize(key);
-				encode(m_buffer,key);
-				encode(m_buffer, _i );
-    			  //cout<< " int " <<endl;
-								cout << "int32" << _i <<endl;
-
-
+              
+                m_size += getVarSize(i);
+                
+                PROTO_TYPE protoType = ( PROTO_TYPE )0;
+                WIRE_TYPE wireType = getWireType ( protoType) ;
+               
+                uint32_t key = getKey ( id, wireType );
+                m_size += getVarSize(key);
+                
+                encode(m_buffer,key);
+                encode(m_buffer, i );
         }
         
         void PROTOSerializer::write ( const uint32_t id, const uint32_t& ui ) {
-                        cout<< " Serialization of uint32_t : "  << endl;
-              //  uint64_t _ui = ui ;
-	//			cout << "htole64"<<endl;
-				m_size += getVarSize(ui);
-	//			cout <<"var size " <<m_size <<endl;
-				PROTO_TYPE protoType = ( PROTO_TYPE )2;
-	//			cout << "PROTO_type " <<protoType<<endl;
-				WIRE_TYPE wireType = getWireType ( protoType) ;
-		//		cout <<"WIRE_TYPE"<<endl;
-				uint32_t key = getKey ( id, wireType );
-		//		cout << "key " << key <<endl;
-				m_size += getVarSize(key);
-	//			cout <<"var size " <<m_size <<endl;
 
-				encode(m_buffer,key);
-		//		cout <<"key encoded " <<endl;
-				encode(m_buffer, ui );
-			//	cout << "number encoded"<<endl;
-				  cout<< " unsigned int" <<ui<< endl;
+                m_size += getVarSize(ui);
+                
+                PROTO_TYPE protoType = ( PROTO_TYPE )2;
+                WIRE_TYPE wireType = getWireType ( protoType) ;
+               
+                uint32_t key = getKey ( id, wireType );
+                m_size += getVarSize(key);
 
-		}
+                encode(m_buffer,key);
+                encode(m_buffer, ui );
+
+
+        }
         
         void PROTOSerializer::write ( const uint32_t id, const float& f ) {
-			//cout<< "Writing id: " << id <<  " of float "  <<endl;
-                //float asdd = f;
-				//void& _f = (uint32_t*) &asdd;
-                //cout << "asd value " << asdd << endl;
-              //  cout << "_f value " << _f <<endl;
-            // Writing id
-            float _f = f;
-         //     char* _f = reinterpret_cast<void*>(test);
-				m_size += 4;
-				PROTO_TYPE protoType = ( PROTO_TYPE )4;
-				WIRE_TYPE wireType = getWireType ( protoType) ;
-				uint32_t key = getKey ( id, wireType );
-				m_size += getVarSize(key);
-				encode(m_buffer,key);
+       
+                float _f = f;
+                m_size += 4;
                 
-               m_buffer.write(reinterpret_cast<const char *>(&_f), 4);
-
-				//m_buffer.write(f,4);
-                // m_buffer << f ;
-	//			cout << "float " << _f<<endl;
-
-
-	   }
+                PROTO_TYPE protoType = ( PROTO_TYPE )4;
+                WIRE_TYPE wireType = getWireType ( protoType) ;
+                
+                uint32_t key = getKey ( id, wireType );
+                m_size += getVarSize(key);
+                
+                encode(m_buffer,key);
+                m_buffer.write(reinterpret_cast<const char *>(&_f), 4);
+       }
 
         void PROTOSerializer::write ( const uint32_t id, const double& d ) {
-			//cout<< " Writing id: " << id << " of double " <<endl;
-			// Writing id
-            double _d = d;
-              
-              //converting double to 
-				m_size += 8;
-				PROTO_TYPE protoType = ( PROTO_TYPE )5;
-				WIRE_TYPE wireType = getWireType ( protoType) ;
-				uint32_t key = getKey ( id, wireType );
-				m_size += getVarSize(key);
-				encode(m_buffer,key);
-                 m_buffer.write(reinterpret_cast<const char *>(&_d), 8);
 
-			//	m_buffer.write( ( char* ) _d, 8);
-				//  cout<< "double " <<_d <<endl;
+                double _d = d;
+              
+                m_size += 8;
+                PROTO_TYPE protoType = ( PROTO_TYPE )5;
+                WIRE_TYPE wireType = getWireType ( protoType) ;
+               
+                uint32_t key = getKey ( id, wireType );
+                m_size += getVarSize(key);
+               
+                encode(m_buffer,key);
+                m_buffer.write(reinterpret_cast<const char *>(&_d), 8);
+
 
         }
         void PROTOSerializer::write ( const uint32_t id, const string& s ) {
-			//cout<< "Writing id: " << id << "of string "  << endl;
-			// Writing id
-			     
-			  // writing string
-			//  void* _s = (string*) &s;
-			  PROTO_TYPE protoType = ( PROTO_TYPE )8;
-			  WIRE_TYPE wireType = getWireType ( protoType) ;
-			  uint32_t key = getKey ( id, wireType );
-			  encode(m_buffer,key);
-			 uint32_t stringSize = 0;
-             stringSize = s.size();
-			  m_size += stringSize;
-                cout << " Encoding string size . which is : " << stringSize << endl;
-			  encode( m_buffer,stringSize );
-              m_buffer.write ( s.c_str(), stringSize+1 );			  //cout<< "string" <<endl;
-			  cout << "string " << s.c_str()<<endl;
+
+              PROTO_TYPE protoType = ( PROTO_TYPE )8;
+              WIRE_TYPE wireType = getWireType ( protoType) ;
+            
+              uint32_t key = getKey ( id, wireType );
+              encode(m_buffer,key);
+              
+              uint32_t stringSize = 0;
+              stringSize = (s.size() + 1) ;
+              m_size += stringSize;
+              
+              encode( m_buffer,stringSize );
+              m_buffer.write ( s.c_str(), stringSize );  
 
 
-
-		}
+        }
 
         void PROTOSerializer::write ( const uint32_t id, const void* data, const uint32_t& size ) {
-			cout<< "Writing id: " << id << "of user data " << data << " size of it " << size << endl;
-		}
-	
+            cout<< "Writing id: " << id << "of user data " << data << " size of it " << size << endl;
+        }
+    
 
-	void PROTOSerializer::write (core::data::Container &container){
-	  cout << "container" <<endl;
-          uint16_t magicNumber = 0xAABB;
-	  uint16_t dataType = container.getDataType();  
-	  encode(m_out, magicNumber);
-      cout << " ------------------------------------------encoded magicNumber " << magicNumber<< endl;
-	  encode(m_out, dataType); 
-      uint32_t msgSize = container.getMessageSize();
-	  m_out << msgSize;
-	  cout << "Container data type " <<  container.getDataType() << endl;
-	  cout << "message size " << msgSize;
-	  m_out << container.m_serializedData.str();
-		// Serializing container
-		// -- write Message size -- uint32_t
-		// -- write Payload
-		// ----encode Key -- uint32_t
-		// ----encode value 
-	}
-	
-	void print_bytes(const void *object, size_t size)
-{
-  size_t i;
+    void PROTOSerializer::write (core::data::Container &container){
+   
+            uint16_t magicNumber = 0xAABB;
+            uint16_t dataType = container.getDataType();  
+            encode(m_out, magicNumber);
+            encode(m_out, dataType); 
+            
+            uint32_t msgSize = container.m_message_size;
+            m_out << msgSize;
+            m_out << container.m_serializedData.str();
+//                 Serializing container
+//                 -- write Message size -- uint32_t
+//                 -- write Payload
+//                 ----encode Key -- uint32_t
+//                 ----encode value 
+    }
+    
 
+
+    
+    void PROTOSerializer::encode( ostream &out, uint64_t value){
   
-  for(i = 0; i < size; i++)
-  {
-    printf("%02x ", ((const unsigned char *) object)[i] & 0xff);
-  }
 
-}
+           value = htole64( value);
+              do {
 
-
-	
-	void PROTOSerializer::encode( ostream &out, uint64_t value){
-                   cout << " printing byte of value  " << value << endl;
-
-	       value = htole64( value);
-           cout << " printing byte of value  " << value << endl;
-		do {printf("[ ");
-
-		    char byte = value & (uint8_t) 0x7F;
-    		    value >>= 7;
-	
-	    	if ( value) {
-		    byte |= ( uint8_t ) 0x80;
-		}
-	     print_bytes(&byte, sizeof(byte));
-		out.put( byte );
+            char byte = value & (uint8_t) 0x7F;
+                value >>= 7;
+    
+            if ( value) {
+            byte |= ( uint8_t ) 0x80;
+        }
+        out.put( byte );
         
-		} while (value);
-         printf("]\n"); 
-	}
+        } while (value);
+    }
 
 
-	uint8_t PROTOSerializer::getVarSize( uint64_t value){
-		uint8_t size = 0;
-		value = htole64(value);
-		do {
-			char byte = value & (uint8_t) 0x7F;
-			value >>=7;
+    uint8_t PROTOSerializer::getVarSize( uint64_t value){
+        uint8_t size = 0;
+        value = htole64(value);
+        do {
+            char byte = value & (uint8_t) 0x7F;
+            value >>=7;
 
-			if(value){
-				byte |= (uint8_t) 0x80;
-			}
-	
-			++size;
-	
-		} while (value);
+            if(value){
+                byte |= (uint8_t) 0x80;
+            }
+    
+            ++size;
+    
+        } while (value);
 
-		return size;
-	}
+        return size;
+    }
 
 
         } 
