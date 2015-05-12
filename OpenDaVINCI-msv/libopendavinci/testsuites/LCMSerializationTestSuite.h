7b78619d14acac68b56173e3892420532f9884b2
Saulius <s.eidukas@gmail.com>
2015-05-12 22:19:52 +0200

    Added more test for Lcm 	modified:   libopendavinci/testsuites/LCMSerializationTestSuite.h

--- OpenDaVINCI-msv/libopendavinci/testsuites/LCMSerializationTestSuite.h
+++ OpenDaVINCI-msv/libopendavinci/testsuites/LCMSerializationTestSuite.h
@@ -14,6 +14,7 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <boost/concept_check.hpp>
 
 #include "core/base/Hash.h"
 #include "core/base/Serializable.h"
@@ -31,7 +32,7 @@ using namespace core::base;
 using namespace core::data;
 using namespace core::data::control;
 
-class SerializationTestNestedData : public Serializable {
+class SerializationTestNestedData : public core::base::Serializable {
     public:
         SerializationTestNestedData() :
                 m_double(0) {}
@@ -41,61 +42,55 @@ class SerializationTestNestedData : public Serializable {
         ostream& operator<<(ostream &out) const {
             SerializationFactory sf;
 
-            LCMSerializer &s = sf.getLCMSerializer(out);
+            Serializer &s = sf.getSerializer(out);
 
-            s.write(1110, m_double);
+            s.write(CRC32 < OPENDAVINCI_CORE_STRINGLITERAL8('m', '_', 'd', 'o', 'u', 'b', 'l', 'e') >::RESULT,
+                    m_double);
+
             return out;
         }
 
         istream& operator>>(istream &in) {
             SerializationFactory sf;
 
-            LCMDeserializer &d = sf.getLCMDeserializer(in);
+            Deserializer &d = sf.getDeserializer(in);
 
-            d.read(0, m_double);
+            d.read(CRC32 < OPENDAVINCI_CORE_STRINGLITERAL8('m', '_', 'd', 'o', 'u', 'b', 'l', 'e') >::RESULT,
+                   m_double);
+
             return in;
         }
 };
 
-class SerializationTestSampleData : public Serializable {
+class SerializationTestSampleData : public core::base::Serializable {
     public:
-         int64_t m_hash;
-        bool m_bool;
-        int32_t m_int;
-        string m_string;
-        float m_float;
-        double m_double;
         SerializationTestSampleData() :
-                m_hash(),
                 m_bool(false),
                 m_int(0),
                 m_string(""),
-                m_float(),
-                m_double() {}
-       SerializationTestSampleData(const SerializationTestSampleData& o):
-          m_hash(o.m_hash),
-                m_bool(o.m_bool),
-                m_int(o.m_int),
-                m_string(o.m_string),
-                m_float(o.m_float),
-                m_double(o.m_double)
-                { }
-       
-     
+                m_nestedData() {}
+
+        bool m_bool;
+        int32_t m_int;
+        string m_string;
+        SerializationTestNestedData m_nestedData;
+
         ostream& operator<<(ostream &out) const {
             SerializationFactory sf;
 
-            LCMSerializer &s = sf.getLCMSerializer(out);
+            Serializer &s = sf.getSerializer(out);
 
-            s.write(110, m_bool);
-            
-            s.write(220, m_int);
+            s.write(CRC32 < OPENDAVINCI_CORE_STRINGLITERAL6('m', '_', 'b', 'o', 'o', 'l') >::RESULT,
+                    m_bool);
 
-            s.write(330, m_string);
-            
-            s.write(330, m_float);
+            s.write(CRC32 < OPENDAVINCI_CORE_STRINGLITERAL8('m', '_', 'n', 'e', 's', 't', 'e', 'd') >::RESULT,
+                    m_nestedData);
 
-            s.write(440, m_double);
+            s.write(CRC32 < OPENDAVINCI_CORE_STRINGLITERAL5('m', '_', 'i', 'n', 't') >::RESULT,
+                    m_int);
+
+            s.write(CRC32 < OPENDAVINCI_CORE_STRINGLITERAL5('m', '_', 's', 't', 'r') >::RESULT,
+                    m_string);
 
             return out;
         }
@@ -103,22 +98,19 @@ class SerializationTestSampleData : public Serializable {
         istream& operator>>(istream &in) {
             SerializationFactory sf;
 
-            LCMDeserializer &d = sf.getLCMDeserializer(in);
-            
-            d.read(CRC32 < OPENDAVINCI_CORE_STRINGLITERAL6('m', '_', 'b', 'o', 'o', 'l') >::RESULT,
-                   m_bool);
-            
-            d.read(CRC32 < OPENDAVINCI_CORE_STRINGLITERAL5('m', '_', 'i', 'n', 't') >::RESULT,
-                   m_int);
+            Deserializer &d = sf.getDeserializer(in);
+
+            d.read(CRC32 < OPENDAVINCI_CORE_STRINGLITERAL8('m', '_', 'n', 'e', 's', 't', 'e', 'd') >::RESULT,
+                   m_nestedData);
 
             d.read(CRC32 < OPENDAVINCI_CORE_STRINGLITERAL5('m', '_', 's', 't', 'r') >::RESULT,
                    m_string);
-            
-            d.read(CRC32 < OPENDAVINCI_CORE_STRINGLITERAL7('m', '_', 'f', 'l', 'o', 'a', 't') >::RESULT,
-                   m_float);
-            
-            d.read(CRC32 < OPENDAVINCI_CORE_STRINGLITERAL8('m', '_', 'd', 'o', 's', 't', 'e', 'd') >::RESULT,
-                   m_double);
+
+            d.read(CRC32 < OPENDAVINCI_CORE_STRINGLITERAL5('m', '_', 'i', 'n', 't') >::RESULT,
+                   m_int);
+
+            d.read(CRC32 < OPENDAVINCI_CORE_STRINGLITERAL6('m', '_', 'b', 'o', 'o', 'l') >::RESULT,
+                   m_bool);
 
             return in;
         }
@@ -127,108 +119,82 @@ class SerializationTestSampleData : public Serializable {
 
 class SerializationTest : public CxxTest::TestSuite {
     public:
-        void testSerializationDeserialization() {
-            // Create some data.
-            SerializationTestSampleData sd;
-            sd.m_bool = true;
-            sd.m_int = 42;
-            sd.m_string = "This is an example.";
-            sd.m_float = -321.456;
-            sd.m_double = -42.42;
-            
-
-            // Create a data sink.
+        void testSerializationNested() {
+            std::cout << " testing nested data " << endl; 
+           SerializationTestSampleData sd;
+           sd.m_bool = true;
+           sd.m_int = 42;
+           sd.m_nestedData.m_double = -42.42;
+           sd.m_string = "This is an example.";
+           
             stringstream inout;
             inout << sd;
             inout.flush();
-            
+
             // Read from the previously created data sink.
             SerializationTestSampleData sd2;
             inout >> sd2;
-            
-            cout << "m_bool Is: " << sd2.m_bool << ", Should: " << true << endl;
+
             TS_ASSERT(sd2.m_bool);
-            cout << "m_int Is: " << sd2.m_int << ", Should: " << 42 << endl;
             TS_ASSERT(sd2.m_int == 42);
-            cout << "m_string Is: " << sd2.m_string << ", Should: " << "This is an example." << endl;
             TS_ASSERT(sd2.m_string == "This is an example.");
-            cout << "m_float Is: " << sd2.m_float << ", Should: " << -321.456 << endl;
-            TS_ASSERT_DELTA(sd2.m_float, -321.456, 1e-5);
-            cout << "m_double Is: " << sd2.m_double << ", Should: " << -42.42 << endl;
-            TS_ASSERT_DELTA(sd2.m_double, -42.42, 1e-5);
+            TS_ASSERT_DELTA(sd2.m_nestedData.m_double, -42.42, 1e-5);
+            cout << sd2.m_bool <<endl;
+            cout << sd2.m_string<<endl;;
+            cout << sd2.m_int<<endl;;
+            cout << sd2.m_nestedData.m_double<<endl;;
+            cout << " end testing nested data " << endl; 
+
+           
         }
         
-          void testDeserialization() {
-              cout << "Testing Container " << endl;
+          void testSerializationContainer() {
+            cout << " Test serializing  container " << endl;
             // Create some data.
-//             SerializationTestSampleData sd;
-//             sd.m_bool = true;
-//             sd.m_int = 42;
-//             sd.m_string = "This is an example.";
-//             sd.m_float = -321.456;
-//             sd.m_double = -42.42;
-//             TimeStamp ts;
+        
               
-              VehicleControl vc;
-              vc.setSpeed(2.0);
-              vc.setAcceleration(5.0);
-              vc.setSteeringWheelAngle(23.0);
-              vc.setBrakeLights(true);
+            VehicleControl vc;
+            vc.setSpeed(2.0);
+            vc.setAcceleration(1.6);
+            vc.setSteeringWheelAngle(32);
+            vc.setBrakeLights(true);
+            vc.setLeftFlashingLights(false);
+            vc.setRightFlashingLights(true);
             cout << vc.toString() <<endl;
               
             Container c(Container::VEHICLECONTROL,vc);
-            
-            stringstream inout2;
-            inout2 << vc;
-            
-            VehicleControl vc3 ;
-            inout2 >> vc3;
-            cout << vc3.toString()<<endl;
-            // Create a data sink.
-       
-           
+          
+
             stringstream inout;
 
-                SerializationFactory sf;
-                LCMSerializer &lcm = sf.getLCMSerializer(inout);
-            lcm.write(c);
-             inout.flush();
+            SerializationFactory sf;
+            LCMSerializer &lcms = sf.getLCMSerializer(inout);
+            lcms.write(c);
+            inout.flush();
             
-              Container c2;
-              LCMDeserializer &lcmd = sf.getLCMDeserializer(inout);
-              lcmd.read(inout, c2);
-            
-              VehicleControl vc2 = c2.getData<VehicleControl>();
-              cout << vc2.toString()<<endl;
-//               inout >> c2;
-              TS_ASSERT(vc2.getBrakeLights());
-              cout << "brake lights are: " << vc2.getBrakeLights() << " Should: " << vc.getBrakeLights()<<endl;
-              TS_ASSERT_DELTA(vc2.getSpeed(),2.0,1e-5);
-              cout << "speed is: " << vc2.getSpeed() << " Should: " << vc.getSpeed() << endl;
-//             // Read from the previously created data sink.
-//             SerializationTestSampleData sd2 = c2.getData<SerializationTestSampleData>();
-// //             inout >> sd2;
-//             
-//             cout << "m_hash Is: " << sd2.m_hash << ", Should: " << "dunno" << endl;
-//             cout << "m_bool Is: " << sd2.m_bool << ", Should: " << false << endl;
-//             TS_ASSERT(sd2.m_bool);
-//             cout << "m_int Is: " << sd2.m_int << ", Should: " << 42 << endl;
-//             TS_ASSERT(sd2.m_int == 42);
-//             cout << "m_string Is: " << sd2.m_string << ", Should: " << "This is an example." << endl;
-//             TS_ASSERT(sd2.m_string == "This is an example.");
-//             cout << "m_float Is: " << sd2.m_float << ", Should: " << -321.456 << endl;
-//             TS_ASSERT_DELTA(sd2.m_float, -321.456, 1e-5);
-//             cout << "m_double Is: " << sd2.m_double << ", Should: " << -42.42 << endl;
-//             TS_ASSERT_DELTA(sd2.m_double, -42.42, 1e-5);
+            Container c2;
+            LCMDeserializer &lcmd = sf.getLCMDeserializer(inout);
+            lcmd.read(inout, c2);
+
+            VehicleControl vc2 = c2.getData<VehicleControl>();
+            cout << vc2.toString()<<endl;
+
+            TS_ASSERT(vc.toString() == vc2.toString());
+
+            cout << " end test of container " << endl;
         }
         
-          void testPayload() {
-              cout << " Test payload " << endl;
-              VehicleControl vc;
-              vc.setSpeed(2.0);
-              vc.setAcceleration(5.0);
-              vc.setSteeringWheelAngle(23.0);
-              vc.setBrakeLights(true);
+          void testSerializationPayload() {
+            cout << " Test payload " << endl;
+            
+            VehicleControl vc;
+            vc.setSpeed(2.0);
+            vc.setAcceleration(1.6);
+            vc.setSteeringWheelAngle(32);
+            vc.setBrakeLights(true);
+            vc.setLeftFlashingLights(false);
+            vc.setRightFlashingLights(true);
+            
             cout << vc.toString() <<endl;
               
             
@@ -238,39 +204,71 @@ class SerializationTest : public CxxTest::TestSuite {
             VehicleControl vc3 ;
             inout2 >> vc3;
             cout << vc3.toString()<<endl;
-            // Create a data sink.
-            TS_ASSERT(vc.toString() == vc3.toString());
-             cout << " end  Test payload " << endl;
+
+             TS_ASSERT(vc.toString() == vc3.toString());
+
+            cout << " end  Test payload " << endl;
        }
         
-        void xtestArraySerialisation() {
-            /*
-            stringstream stream;
-            uint32_t array[10];
-            for (uint32_t i=0; i<10; ++i) {
-                array[i] = i;
-            }
-
-            SerializationFactory sf;
-            LCMSerializer& s = sf.getLCMSerializer(stream);
-            s.write(550, &array, 10);
-            
-            stream.clear();
-            stream.seekg(0, ios_base::beg);
-            
-            LCMDeserializer& d = sf.getLCMDeserializer(stream);
-            //int64_t hash;
-            //d.read(0, hash);
-            uint32_t deserializedArray[10];
-            d.read(1, &deserializedArray, 10);
-
-            for (uint32_t i=0; i<10; ++i) {
-                TS_WARN(deserializedArray[i] == i);
-                cout << "Is: " << deserializedArray[i] << ", Should: " << i << endl;
-            }
-            */
+        void xtestLcmSerialisation() {
+          cout << " LCM serialisation test " << endl;
+          char* rawData; // need assign raw data somehow
+          stringstream rawDataStream ;
+          rawDataStream << rawData;
+           VehicleControl vc;
+           vc.setSpeed(2.0);
+           vc.setAcceleration(1.6);
+           vc.setSteeringWheelAngle(32);
+           vc.setBrakeLights(true);
+           vc.setLeftFlashingLights(false);
+           vc.setRightFlashingLights(true);
+           
+           stringstream inout;
+          
+           Container c(Container::VEHICLECONTROL,vc);
+           
+           SerializationFactory sf;
+           LCMSerializer &lcms = sf.getLCMSerializer(inout);
+           
+           lcms.write(c);
+           
+           TS_ASSERT(rawDataStream.str() == inout.str());
+          
+           cout << " End of the lcm serialisation test " << endl;
+        
         }
         
+        void xtestLcmDeserialisation() {
+            
+           cout << " lcm Deserialisation test " << endl;
+           char* rawData;// need assign raw data somehow
+           stringstream rawDataStream ;
+           rawDataStream << rawData;
+  
+           VehicleControl vc;
+           vc.setSpeed(2.0);
+           vc.setAcceleration(1.6);
+           vc.setSteeringWheelAngle(32);
+           vc.setBrakeLights(true);
+           vc.setLeftFlashingLights(false);
+           vc.setRightFlashingLights(true);
+           
+           cout << vc.toString() << endl;
+           
+           SerializationFactory sf;
+           LCMDeserializer &lcmd = sf.getLCMDeserializer(rawDataStream);
+           Container c;
+           lcmd.read(rawDataStream,c);
+           
+           VehicleControl vc2 = c.getData<VehicleControl>();
+           cout << vc2.toString()<<endl;
+
+           TS_ASSERT(vc.toString() == vc2.toString());
+          
+           cout << " End of lcm Deserialisation test" <<endl;
+        }
+        
+        void xtestContainerNestedDataSerialisation() {}
 };
 
 #endif /*CORE_LCMSERIALIZATIONTESTSUITE_H_*/
